437b74e7e5d429ab85ff92ac43b14a0d8cc3b2da
Guillermo Rubio Gutierrez <guillermorubiogutierrez@MacBook-Air-de-Guillermo.local>
2022-05-18 18:54:59 +0200

    Deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import './styles/styles.scss';
-import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
